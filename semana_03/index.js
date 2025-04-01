26d244f2a45491738366e76a6508843582dfa27b
nancy.correa@davinci.edu.ar <nancy.correa@davinci.edu.ar>
2025-04-01 10:28:03 -0300

    Semana 3

---
+++ semana_03/index.js
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import express from "express";
+const port = 5000;
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log('Ruta raíz');
+    response.send('Home');
+})
+
+app.get('/products', (request, response) => {
+    const list = ({id: 1, name: "Mouse", price: 2300});
+    response.json(list);
+})
+
+app.post('/products' , (request, response) => {
+    console.log('POST');
+    response.json({id: 2})
+})
+
+app.listen(port, () => {
+    console.log( chalk.green(`Servidor web en el puerto ${port}`));
+})
+
+
+console.log( chalk.green('Puerto' + port));
