0f071c4112ddc215b3eb5d833b2e099f295d8574
nancy.correa@davinci.edu.ar <nancy.correa@davinci.edu.ar>
2025-04-01 10:46:55 -0300

    Cambios actividad

--- semana_03/index.js
+++ semana_03/index.js
@@ -1,9 +1,13 @@
 import chalk from "chalk";
 import express from "express";
+import ProductManager from "./ProductManager";
 const port = 5000;
 const app = express();
 
-app.get('/', (request, response) => {
+const admin = new ProductManager();
+
+
+/*app.get('/', (request, response) => {
     console.log('Ruta raíz');
     response.send('Home');
 })
@@ -13,9 +17,16 @@ app.get('/products', (request, response) => {
     response.json(list);
 })
 
+
 app.post('/products' , (request, response) => {
     console.log('POST');
     response.json({id: 2})
+})
+*/
+
+app.get('/api/products', (request, response) => {
+    const list = ({id: 1, name: "Mouse", price: 2300});
+    response.json(list);
 })
 
 app.listen(port, () => {
