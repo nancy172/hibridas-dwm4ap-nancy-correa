7f99a629d65624d0e11a8562076e92c934d8fcb2
Nancy Correa <nancy.correa@davinci.edu.ar>
2025-05-13 10:44:00 -0300

    Semana 09 - React

---
+++ semana_09/app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let nombre = 'React';
+
+  return (
+
+    <>
+      <h1>Semana 09 { nombre }</h1>
+      <p>Sintaxis JSX</p>
+      <img src="foto.png" alt="foto" />
+    </>
+  )
+}
+
+export default App
