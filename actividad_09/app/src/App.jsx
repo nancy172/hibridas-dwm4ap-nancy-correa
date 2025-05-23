d9ebf65b9a6bce3c0500205b53b91a4b6c728e12
Nancy Correa <nancy.correa@davinci.edu.ar>
2025-05-23 10:46:25 -0300

    Actividad 9 - modificación TodoApp

--- actividad_09/app/src/App.jsx
+++ actividad_09/app/src/App.jsx
@@ -1,34 +1,9 @@
-import Header from './components/Header';
-import TodoList from './components/TodoList';
-import TodoItem from './components/TodoItem';
-import Footer from './components/Footer';
-import './App.css'
+import TodoApp from './components/TodoApp';
+import './App.css';
 
 function App(){
-
-    const tareas = [
-        {id:1, nombre: "Estudiar React", completo: true},
-        {id:2, nombre: "Regar plantas", completo: false},
-        {id:3, nombre: "Turno con el dentista", completo: true},
-        {id:4, nombre: "Ver serie", completo: true},
-        {id:5, nombre: "Salir a correr", completo: false}
-    ]
-
     return(
-        <>
-            <Header/>
-            <main>
-                {
-                    tareas.length > 0 ? (<h3> Hay {tareas.length} tareas</h3>) : (<h3>No hay tareas</h3>)
-                }
-                <TodoList>
-                    {
-                    tareas.map( tarea => <TodoItem nombre={tarea.nombre} completo={tarea.completo} key={tarea.id} />)
-                    }
-                </TodoList>
-            </main>
-            <Footer/>
-        </>
+        <TodoApp/>
     )
 }
 
