ca8ae8229ccb73df49badd58cda4340bd8609ec2
Nancy Correa <nancy.correa@davinci.edu.ar>
2025-06-03 00:47:21 -0300

    localStorage y fetch a la API

--- semana_11/app/src/App.jsx
+++ semana_11/app/src/App.jsx
@@ -8,16 +8,26 @@ import Footer from './components/Footer';
 
 function App(){
 
-    // Estados
-    const [ cantidad, setCantidad ] = useState(5); // defino el valor inicial de cantidad
-    const [ user, setUser] = useState( {name: 'Lucas', age:22} );
-    const [ tareas, setTareas] = useState([
+    /*localStorage.setItem('tareas', JSON.stringify([
         {id:1, nombre: "Estudiar React", completo: true},
         {id:2, nombre: "Regar plantas", completo: false},
         {id:3, nombre: "Turno con el dentista", completo: true},
         {id:4, nombre: "Ver serie", completo: true},
         {id:5, nombre: "Salir a correr", completo: false}
-    ])
+    ]));
+    */
+
+    // Estados, siempre se definen en un nivel superior nunca dentro de un if o bucle
+    const [ cantidad, setCantidad ] = useState(5); // defino el valor inicial de cantidad
+    const [ user, setUser] = useState( {name: 'Lucas', age:22} );
+    const [ tareas, setTareas] = useState( () => {
+
+        console.log("Inicio del estado");
+        const lista = JSON.parse(localStorage.getItem('tareas'));
+
+        return lista ? lista : [];
+    })
+    const [tarea, setTarea] = useState({ id: 0, nombre:'', completo: true})
  
 
     function sumar(){
@@ -33,20 +43,28 @@ function App(){
         setUser( {...user, age: 31} ); // Se actualiza solo la edad
     }
 
-    function addTarea(){
-        const nombre = prompt('Que tarea quiere agregar?');
-        const newTarea = {id:6, nombre: nombre, completo: false};
+    function addTarea(e){
+        e.preventDefault();
+        const id = crypto.randomUUID();
+        const newTarea = {id, nombre: tarea.nombre, completo: false};
         setTareas( [...tareas, newTarea] );
+        // Escribo el localStorage o se realiza el fetch a la API del tipo POST
+
+        // Borramos el valor del estado
+        setTarea( {...tarea, nombre: ''});
     }
 
     const cambiar = (id) => {
         console.log(`Desde el Padre ${id}`)
     }
 
-    // <div id="add">
-    //                 <input type="text" placeholder="Escriba la tarea..." />
-    //                 <button type="button">+ Agregar</button>
-    //             </div>
+    async function getApi(){
+        const response = await fetch('https://fakestoreapi.com/users');
+        const data = await response.json();
+        console.log(data);
+        // Hacer fetch a la api o a un json local
+    }
+
 
     return(
         <>
@@ -62,7 +80,18 @@ function App(){
                 <h4>Cantidad de tareas: {cantidad} </h4>
                 <button type="button" onClick={cambiarEstado} >Cambiar estado</button>
                 <button type="button" onClick={sumar} >Sumar</button>
-                <button type="button" onClick={addTarea} >Crear tarea</button>
+                <hr />
+                {/* Formulario */}
+                <form onSubmit={addTarea}>
+                    <label htmlFor="">Descripción</label>
+                    <input 
+                        type="text" 
+                        value={ tarea.nombre }
+                        onChange={ (e) => setTarea( {...tarea, nombre: e.target.value} ) }
+                    />
+                    <button type="submit">Crear tarea</button>
+                </form>
+                
                 {
                     tareas.length > 0 ? (<h3> Hay tareas</h3>) : (<h3>No hay tareas</h3>)
                 }
